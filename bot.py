57c184842b739828ed07ba948284e2fb6c5f5458
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2022-01-11 13:06:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,7 @@ async def link_handler(bot, message):
     link = message.matches[0].group(0)
     try:
         short_link = await get_shortlink(link)
-        await message.reply(f'Here is your \n\n
-{short_link}', quote=True)
+        await message.reply(f'Here is your Short Link :- ({short_link})', quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
